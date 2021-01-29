51f7b5343b82ae315d9cb55b20f60a3d7c6afea0
unknown <yvonnegamboeck@web.de>
2021-01-29 14:45:19 +0100

    responsive menu

--- JS/sidebar.js
+++ JS/sidebar.js
@@ -4,14 +4,14 @@ let currentNavigation = ['Board', 'Backlog', 'Add Task', 'Help', 'My Profile', '
  * 
  */
 function loadSideBar() {
-    document.getElementById('sidebar').innerHTML += `<div class="sidebar">
+    document.getElementById('sidebar').innerHTML += `<div class="sidebar d-flex-responsive">
 
     <div class="sidebar-menu">
         <div class="sidebar-logo">
             <img src="./img/joinlogo.PNG">
         </div>
 
-        <div id="sidebar-navigation" class="sidebar-navigation d-flex">
+        <div id="sidebar-navigation" class="sidebar-navigation d-flex d-none-responsive">
         <div class="d-flex navigation-row"> <a href="board.html">  Board</a>  </div>
         <div class="d-flex navigation-row"> <a href="backlog.html">Backlog</a> </div>
         <div class="d-flex navigation-row"> <a href="addtask.html">Add Task</a> </div>
@@ -23,14 +23,61 @@ function loadSideBar() {
 
     </div>
 
-    <div class="sidebar-profile">
+    <div class="sidebar-profile d-none-responsive">
         <img src="img/pretty-smiling-joyfully-female-with-fair-hair-dressed-casually-looking-with-satisfaction.jpg">
         <button>Ausloggen</button> 
     </div>
 
-</div>`
+    <div class="hamburger-menu"> <img onclick="menuOpen()" src="img/hamburger-menu.png"></div>
 
+  </div>
+  `
 }
+
+/**
+ * When click on the 3 stripes the mobile menu is opening
+ */
+function openMobileMenu() {
+    document.getElementById('total-size').innerHTML = `
+    <div class="box-transparent" onclick="menuClose()">
+    <div class="responsive-menu">
+    <div class="d-flex navigation-row"> <a href="board.html">  Board</a>  </div>
+    <div class="d-flex navigation-row"> <a href="backlog.html">Backlog</a> </div>
+    <div class="d-flex navigation-row"> <a href="addtask.html">Add Task</a> </div>
+    <div class="d-flex navigation-row"> <a href="help.html">  Help</a> </div>
+    <div class="d-flex navigation-row"> <a href="myprofile.html">   My Profile</a> </div>
+    <div class="d-flex navigation-row"> <a href="Impressum.html">   Impressum</a> </div>
+    <div class="d-flex navigation-row"> <a href="datenschutz.html">   Datenschutz</a> </div>
+
+    <div class="sidebar-profile">
+    <img src="img/pretty-smiling-joyfully-female-with-fair-hair-dressed-casually-looking-with-satisfaction.jpg">
+    <button>Ausloggen</button> 
+</div>
+    </div>
+
+
+
+    </div>
+    `
+}
+/**
+ * mobile menu close
+ */
+function menuClose() {
+    document.getElementById('total-size').classList.add("d-none-responsive");
+}
+
+/**
+ * mobile menu is opening
+ */
+function menuOpen() {
+    document.getElementById('total-size').classList.remove("d-none-responsive");
+    openMobileMenu();
+}
+
+
+
+
 
 function saveBorder() {
     let whiteborder = document.querySelector(".navigation-row");
@@ -39,7 +86,6 @@ function saveBorder() {
         this.classList.toggle("is-active");
     });
 }
-
 
 
 
@@ -52,13 +98,6 @@ function saveBorder() {
     }
 }   */
 
-/* function addCurrentNavigation() {
-    for (let index = 0; index < currentNavigation; index++) {
-
-        document.getElementById('sidebar-navigation').innerHTML += `
-    <div class="d-flex navigation-row"> <div id="opacity" class="hover-square opacity"></div> <a href="board.html onclick="addSquare()">  ${currentNavigation[index]} </a>  </div>
-    `}
-}  */
 
 /* <div id="sidebar-navigation" class="sidebar-navigation d-flex">
         <div class="d-flex navigation-row"> <div id="opacity" class="hover-square opacity"></div> <a href="board.html" onclick="addSquare()">  ${currentNavigation}</a>  </div>
