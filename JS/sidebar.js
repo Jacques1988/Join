8bec4e88f229debd4fad19a865cf22d124307b65
unknown <jacquesvanluyck@gmail.com>
2021-02-02 15:53:45 +0100

    fixed color logout button

--- JS/sidebar.js
+++ JS/sidebar.js
@@ -23,7 +23,7 @@ function loadSideBar() {
 
     <div class="sidebar-profile d-none-responsive">
         <a href="myprofile.html" style="margin-bottom: 8px"> <img src="img/pretty-smiling-joyfully-female-with-fair-hair-dressed-casually-looking-with-satisfaction.jpg"></a>
-        <a class="logoutlink" href="logout.html"><div class="headline2 logoutbtn" onclick="logout()"><div class="logoutbtnanimatediv"><span>Logout</span></div></div></a> 
+        <a class="logoutlink" href="logout.html"><div class="logoutbtn" onclick="logout()"><div class="logoutbtnanimatediv"><span>Logout</span></div></div></a> 
 
     </div>
 
