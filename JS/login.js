24db39fe93c5a525f8f983c5b0001c1e76955f5c
JohannesNissenGit <nissenjohannes@web.de>
2021-02-03 09:29:23 +0100

    password check easier solution

--- JS/login.js
+++ JS/login.js
@@ -1,32 +1,17 @@
-let loginsuccess = false;
 
-/**
- * login(): General login function. 
- */
-
-function login() {
-    loginsuccess = false;
-    searchuser();
-       
-}
 
 /*
 *checks if loginsuccess condition is true and proceeds to main page. 
 */
-function checksuccess() {
-if (loginsuccess = true) {
+function loginsuccess() {
+
     window.open('board.html', '_self');
-        loginsuccess = false;
-    }
-    else{
-        loginsuccess = false;
-       }
 }
 
 /**
  * searchuser(): checks if input user exists
  */
-function searchuser() {
+function login() {
     let usernametry = document.getElementById('nameinput').value;
     let condition = users.findIndex(std => std.username === usernametry); //https://medium.com/tinytute/how-to-get-index-from-a-json-object-with-value-javascript-a556f93c0194
 
@@ -35,14 +20,14 @@ function searchuser() {
     }
     else {
         alert('User doesn´t exist.');
-        loginsuccess = false;
+
     }
 }
 
 /**
  * checkpassword: checks, if password matches selected user
  * @param {} a : selected user that exists.
- *  @param {} a : selected user's id'
+ *  @param {} b : selected user's id'
  */
 
 function checkpassword(a, b) {
@@ -52,11 +37,11 @@ function checkpassword(a, b) {
 
     if (passwordtry === users[idpassword].userpassword) {
         console.log('correct PW') //TEST. PLZ REMOVE LATER
-        loginsuccess = true;
-        checksuccess();
+
+        loginsuccess();
     }
     else {
         alert('Incorrect password.');
-        loginsuccess = false;
+
     }
 }
