91b1686fa2fdea2d7d3e1c3e029783defda73f02
unknown <jacquesvanluyck@gmail.com>
2021-02-04 13:43:42 +0100

    update encryption and decryption userpassword

---
+++ JS/encrypt/encrypt&decryptPW.js
@@ -0,0 +1,18 @@
+//here begins encrypt and decryption
+
+let crypt = {
+    secret: users['userpassword'],
+
+
+    encrypt: function(clear) {
+        let cipher = CryptoJS.AES.encrypt(clear, crypt.secret);
+        cipher = cipher.toString();
+        return cipher;
+    },
+
+    decrypt: function(cipher){
+        var decipher = CryptoJS.AES.decrypt(cipher, crypt.secret);
+        decipher = decipher.toString(CryptoJS.enc.Utf8);
+        return decipher;
+    }
+};
