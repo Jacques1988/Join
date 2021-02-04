f9157985b15dfd2c14d99128d7c14102a84ee510
JohannesNissenGit <nissenjohannes@web.de>
2021-02-04 20:01:45 +0100

    adduser delete/view update

--- JS/addtask.js
+++ JS/addtask.js
@@ -1,14 +1,18 @@
 /**
  * updateusers: displays those users who are assigned to task.
  */
-/*function updateusers() {
-    for (i=0; i< currenttaskusers.length; i++) {
+function updatetaskusers() {
 
+    document.getElementById('usersassignedtotask').innerHTML = '';
+
+    for (i = 0; i < currenttaskusers.length; i++) {
+        let currentuserid = currenttaskusers[i];
+        console.log(currentuserid);
+        let userimage = users[currentuserid].userimage; //hier base64 oder verbindung zum uploadnamen
+        document.getElementById('usersassignedtotask').innerHTML += `<img src="./img/${userimage}" alt="" class="" onclick="deleteuserfromtask(${i})"></img>`;
     }
-
-
 }
-*/
+
 
 /**
  * assignuser: shows available users to assign task to
@@ -16,11 +20,12 @@
 function assignuser() {
 
     document.getElementById('addtaskuserlist').classList.remove('d-none');
-    document.getElementById('addtaskuserlist').innerHTML = '';
+    document.getElementById('addtaskuserlist').classList.add('z-2');
+    document.getElementById('addtaskuserlist').innerHTML = 'Available Users:';
 
     for (let i = 0; i < users.length; i++) {
         let user = users[i];
-        document.getElementById('addtaskuserlist').innerHTML += `<div class="" id="assignuserlist${user['userid']}" onclick="selectuserassign(${user['userid']})">
+        document.getElementById('addtaskuserlist').innerHTML += `<div id="assignuserlist${user['userid']}" onclick="selectuserassign(${user['userid']})">
         <img src="./img/${user['userimage']}" ></div>`;
 
     }
@@ -38,11 +43,22 @@ function selectuserassign(id) {
     }
     else {
         document.getElementById('addtaskuserlist').classList.add('d-none');
+        document.getElementById('addtaskuserlist').classList.remove('z-2');
         let selecteduser = users[id].userid;
         currenttaskusers.push(selecteduser); //pushes userid into currenttaskusers array 
         let PushTaskToUser = users[id].usertasks;
-        PushTaskToUser.push(newtaskid); //pushes taskid into user array
+        PushTaskToUser.push(newtaskid); //pushes taskid into user array (HIER STIMMT NOCH ETWAS NICHT, DAS MUSS ERST IN SERVERFUNCTIONS PASSIEREN!)
 
-        updateusers();
+        updatetaskusers();
     }
+}
+
+/**
+ * deleteuser: deletes user from currenttasks and initiates view update; Important: This function does not use userid, instead it uses current position in array
+ */
+
+function deleteuserfromtask(position) {
+    currenttaskusers.splice(position, 1);
+    updatetaskusers();
+
 }
