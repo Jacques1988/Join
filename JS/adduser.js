8da3947a5643a6aaf66b4d8e0a4291a40979d357
unknown <yvonnegamboeck@web.de>
2021-02-07 11:06:42 +0100

    fileupload

--- JS/adduser.js
+++ JS/adduser.js
@@ -7,30 +7,31 @@ let images = [];
     formData.append("fileToUpload", fileToUpload);
 }
  */
-const toBase64 = file => new Promise((resolve, reject) => {
-    const reader = new FileReader();
-    reader.readAsDataURL(file);
-    reader.onload = () => resolve(reader.result);
-    reader.onerror = error => reject(error);
-});
+
+ /**
+  * This function converts the uploaded file to a string using Base64 and the file is displayed, 
+  * so the user can see if the upload was successfully
+  */
 async function uploadImage() {
+    const toBase64 = file => new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = () => resolve(reader.result);
+        reader.onerror = error => reject(error);
+    });
+    
     const file = document.getElementById('fileToUpload').files[0];
     console.log(await toBase64(file));
     document.getElementById('myimage').src = await toBase64(file);
+
+    pushImagetoArray(); 
 }
 
-function uploadImage() {
-images.push('http://yvonne-gamboeck.developerakademie.com/Join/uploads/' + fileToUpload.value.split('\\')[2]);
+function pushImagetoArray() {
+file.push(images);
 alert('Datei wurde erfolgreich hochgeladen'); 
 }
 
-/* let images = document.getElementById("fileToUpload").files[0];
-let formData = new FormData();
-formData.append("fileToUpload", images);
-fetch('upload.php', { method: "POST", body: formData });
-
-images.push('http://yvonne-gamboeck.developerakademie.com/Instagram/uploads/' + fileToUpload.value.split('\\')[2]);
-alert('Datei wurde erfolgreich hochgeladen'); */
 
 /**
  * function ContinueToMain: waits and sends user to mainpage
