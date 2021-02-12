1ff80161ec80bf8c91b5b20244b93281b26c2a25
JohannesNissenGit <nissenjohannes@web.de>
2021-02-12 17:07:39 +0100

    active user added to server

--- JS/Server_database.js
+++ JS/Server_database.js
@@ -26,36 +26,36 @@ let passwordHash = CryptoJS.SHA256(password);
 let passwordHashString = passwordHash.toString(CryptoJS.enc.Base64);
 
 let users = [
-    {
-        "userid": 0,
-        "username": 'Name0',
-        "usermail": 'Mailadresse0',
-        "userphone": 'phonenumber0',
-        "userdepartment": 'department0',
-        "userposition": 'position0',
-        "useroffice": 'office0',
-        "userpassword": passwordHashString,
-       // "userdescription": 'Heidiho 0',
-        "userimage": 'user_default-forLinuxServer.JPG',
-        "usercategory": 'default',
-        "usertasks": [0,1]
-    },
-    {
-        "userid": 1,
-        "username": 'Name1',
-        "usermail": 'Mailadresse1',
-        "userphone": 'phonenumber1',
-        "userdepartment": 'department1',
-        "userposition": 'position1',
-        "useroffice": 'office1',
-        "userpassword": 'password1',
-      //  "userdescription": 'Heidiho 1',
-        "userimage": 'user_default-forLinuxServer.JPG',
-        "usercategory": 'default',
-        "usertasks": [1]
-    }];
+  {
+    "userid": 0,
+    "username": 'Name0',
+    "usermail": 'Mailadresse0',
+    "userphone": 'phonenumber0',
+    "userdepartment": 'department0',
+    "userposition": 'position0',
+    "useroffice": 'office0',
+    "userpassword": passwordHashString,
+    // "userdescription": 'Heidiho 0',
+    "userimage": 'user_default-forLinuxServer.JPG',
+    "usercategory": 'default',
+    "usertasks": [0, 1]
+  },
+  {
+    "userid": 1,
+    "username": 'Name1',
+    "usermail": 'Mailadresse1',
+    "userphone": 'phonenumber1',
+    "userdepartment": 'department1',
+    "userposition": 'position1',
+    "useroffice": 'office1',
+    "userpassword": 'password1',
+    //  "userdescription": 'Heidiho 1',
+    "userimage": 'user_default-forLinuxServer.JPG',
+    "usercategory": 'default',
+    "usertasks": [1],
+  }];
 
-    /*
+/*
 *alltasks: array stored on server and here
 *structure elements: taskid, taskname, taskdate, taskcategory, taskstatus, taskurgency, taskdescription, taskusers
 */
@@ -63,25 +63,27 @@ let users = [
 
 //example alltasks (testing)) ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////TESTING ONLY
 let alltasks = [
-    {
-        "taskid": 0,
-        "taskname": 'yvonnes task',
-        "taskdate": 'undefined',
-        "taskcategory": 'Sales',
-        "taskstatus" : 'todo',
-        "taskurgency": 'low',
-        "taskdescription": 'Taskdescription',
-        "taskusers": [0]
-      },
-      {
-        "taskid": 1,
-        "taskname": 'task2',
-        "taskdate": 'undefined',
-        "taskcategory": 'Marketing',
-        "taskstatus" : 'testing',
-        "taskurgency": 'middle',
-        "taskdescription": 'testing,middle,marketing',
-        "taskusers": [0,1]
-      }
+  {
+    "taskid": 0,
+    "taskname": 'yvonnes task',
+    "taskdate": 'undefined',
+    "taskcategory": 'Sales',
+    "taskstatus": 'todo',
+    "taskurgency": 'low',
+    "taskdescription": 'Taskdescription',
+    "taskusers": [0],
+    "taskauthorid": 0
+  },
+  {
+    "taskid": 1,
+    "taskname": 'task2',
+    "taskdate": 'undefined',
+    "taskcategory": 'Marketing',
+    "taskstatus": 'testing',
+    "taskurgency": 'middle',
+    "taskdescription": 'testing,middle,marketing',
+    "taskusers": [0, 1],
+    "taskauthorid": 1
+  }
 
 ];
