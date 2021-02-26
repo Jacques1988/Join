6d8342a3b9a286830f07be2deeb268969c53baf3
JohannesNissenGit <nissenjohannes@web.de>
2021-02-26 12:33:38 +0100

    server jo links

--- JS/adduser.js
+++ JS/adduser.js
@@ -9,7 +9,7 @@ async function uploadImage() {
   let formData = new FormData();
   formData.append("fileToUpload", photo);
   //await fetch('http://yvonne-gamboeck.developerakademie.com/Join/upload.php', { method: "POST", body: formData });   //Yvonne Server
-  await fetch('http://@johannes-nissen.developerakademie.com/Modul10_JoinGruppenarbeit/Join/upload.php', { method: "POST", body: formData });   //Johannes Server
+  await fetch('http://johannes-nissen.developerakademie.com/Join/upload.php', { method: "POST", body: formData });   //Johannes Server
   user = localStorage.getItem('activeuser');
   if (photo) {
     users[user]['userimage'] = photo.name;
